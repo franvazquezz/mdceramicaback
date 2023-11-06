2ec868cd74e43cc349bf25a7f1e7f243d3dbe671
franvazquezz <franvazquez2001@gmail.com>
2023-11-06 10:09:32 -0300

    deploy 5

--- src/db.js
+++ src/db.js
@@ -8,6 +8,13 @@ const {
 const sequelize = new Sequelize(DB_DEPLOY, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  ssl: true, // Asegúrate de tener esta línea
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // Puedes ajustar esto según tus necesidades
+    },
+  }
 });
 // before----
 // const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`, {
